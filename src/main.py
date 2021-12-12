bec575fa5dde2bf3d8404e11716df1169a7d7bd2
samuelPoulin3 <samuel.poulin.1@hotmail.com>
2021-12-12 16:47:16 -0500

    Exclude pickle stuff and run on multiprocessing

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from plugins.SplitData import Split_Data
 from tqdm import tqdm
 import concurrent.futures
 import numpy as np
+import pickle
 
 def worker(image, key, size):
     m = SIFT_Implementation(image, key, size).keypoints
@@ -39,12 +40,20 @@ if __name__ == '__main__':
                 key = df['image_no'][0].astype(str)
                 keypoints[key] = f.result()
 
+    # a_file = open("data.pkl", "wb")
+    # pickle.dump(keypoints, a_file)
+    # a_file.close()
+
+    a_file = open("data.pkl", "rb")
+    keypoints = pickle.load(a_file)
+    a_file.close()
+    
     # ------------------------ PREPARE DATA FOR TRAINING ------------------------
     training_ratio = .8
     test_ratio = .2
     train_data, train_label, test_data, test_label = Split_Data(subjects['data'], keypoints, training_ratio, test_ratio)
     pos_train = np.vstack((train_data['x'], train_data['y'])).T.astype(int)
-    
+
     # import numpy as np
     # import nibabel as nib
 
